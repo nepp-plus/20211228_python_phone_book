ebef0325105757d87278027db46b3a8648bd4166
nepp-plus <nepp.plus@gmail.com>
2021-12-28 15:49:52 +0900

    별도 주석 갈래 (branch) 추가

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from phone_book import print_menu, add_phone_num, show_all_phone_num
 from time import sleep
 
 while True:
+    
+    # 메뉴 출력 기능 -> 입력값이 뭔지 알려줌.
+    # num에 담아두고 활용할 준비.
     num = print_menu()
 
     if num == 0:
